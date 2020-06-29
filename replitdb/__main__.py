4b5e9ef166ff24bdb3a7429991eb1c5018b24271
Codemonkey51 <contact@codemonkey51.dev>
2020-06-29 15:09:06 -0700

    0.0.9

---
+++ replitdb/__main__.py
@@ -0,0 +1,24 @@
+import replitdb.commands
+def main():
+    import sys
+    import os
+    del sys.argv[0]
+    url = False
+    setUrl = os.environ['REPLIT_DB_URL']
+    args = []
+    for i in sys.argv:
+      if(url):
+        url = False
+        setUrl = i
+      elif(i.lower()=='-url'):
+        url = True
+      else:
+        args.append(i)
+    try:
+      replitdb.commands.commandHandler(args,setUrl)
+    except IndexError:
+      print('Too little arguments entered (requesrs at leat 2)')
+
+
+if __name__ == '__main__':
+    main()
